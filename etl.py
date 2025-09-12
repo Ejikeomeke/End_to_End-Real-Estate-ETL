2002fba2865a0c8f401d589a5cf42a9ea11f43e6
Ejikeomeke <engromeke@gmail.com>
2025-09-12 17:31:56 +0100

    date transformation function

--- etl.py
+++ etl.py
@@ -6,12 +6,13 @@ import logging
 from dotenv import load_dotenv
 import os
 
+
 #logging configuration
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode='a',
                     format="%(asctime)s-%(levelname)s - %(message)s")
 
-#Data extractions function
-    
+
+#Data extractions function    
 def extract_data():
     try:
         url = "https://api.rentcast.io/v1/listings/sale?city=Austin&state=TX&status=Active&limit=500"
@@ -27,3 +28,14 @@ def extract_data():
         return raw_data
     except requests.exceptions.RequestException as err:
         logging.info(err)
+        
+
+# function to date transformation
+def convert_year(year):
+    if year is None or not str(year).strip():  # Check for None or empty string
+        return None  
+    try:
+        return date(int(year), 1, 1)
+    except ValueError as err:
+        logging.info(err)
+        return None  # Handle other invalid inputs  
