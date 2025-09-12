4688442580bc56ae1bf07a2627fdbc0d0317c72a
Ejikeomeke <engromeke@gmail.com>
2025-09-12 17:19:25 +0100

    extraction_completed

---
+++ etl.py
@@ -0,0 +1,29 @@
+import psycopg2
+from datetime import datetime
+import json
+import requests
+import logging
+from dotenv import load_dotenv
+import os
+
+#logging configuration
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode='a',
+                    format="%(asctime)s-%(levelname)s - %(message)s")
+
+#Data extractions function
+    
+def extract_data():
+    try:
+        url = "https://api.rentcast.io/v1/listings/sale?city=Austin&state=TX&status=Active&limit=500"
+        API_key = os.getenv("API_KEYS")
+        headers = {
+            "accept": "application/json",
+            "X-Api-Key":API_key
+        }
+
+        response = requests.get(url, headers=headers)
+        
+        raw_data = response.json()
+        return raw_data
+    except requests.exceptions.RequestException as err:
+        logging.info(err)
