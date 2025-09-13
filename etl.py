65d968b805f13af6482917796a623d25f9caad75
Ejikeomeke <engromeke@gmail.com>
2025-09-13 09:01:18 +0100

    logged properly

--- etl.py
+++ etl.py
@@ -147,50 +147,49 @@ def database_connection():
 
 # creating database schema, tables and relationship
 def create_tables():
-    conn = database_connection()
-    cur = conn.cursor()
-    
-    create_table_querry =   """
-                                CREATE SCHEMA IF NOT EXISTS royal_realtors;
-                                DROP TABLE IF EXISTS royal_realtors.property_listing CASCADE;
-                                DROP TABLE IF EXISTS royal_realtors.property_specification CASCADE;
-                                DROP TABLE IF EXISTS royal_realtors.property_location CASCADE;
-                                
-                                CREATE TABLE royal_realtors.property_location(
-                                    location_id SERIAL PRIMARY KEY,
-                                    address VARCHAR (300),
-                                    city VARCHAR (150),
-                                    county VARCHAR (150),
-                                    latitude FLOAT,
-                                    longitude FLOAT);
-                                    
-                                
-                                
-                                CREATE TABLE royal_realtors.property_specification(
-                                    property_id VARCHAR PRIMARY KEY,
-                                    location_id SERIAL,
-                                    citnumber_of_rooms INTEGER,
-                                    number_of_bathrooms INTEGER,
-                                    property_size FLOAT,
-                                    year_built DATE,
-                                    property_type VARCHAR(100),
-                                    lot_size FLOAT,
-                                    FOREIGN KEY (location_id) REFERENCES royal_realtors.property_location(location_id)
-                                );
-                                
-                                
-                                CREATE TABLE royal_realtors.property_listing(
-                                    listing_id SERIAL PRIMARY KEY,
-                                    property_id VARCHAR,
-                                    price FLOAT,
-                                    listing_type  VARCHAR(150),
-                                    listed_date DATE,
-                                    status VARCHAR(50),
-                                    FOREIGN KEY (property_id) REFERENCES royal_realtors.property_specification(property_id)
-                                );
-                                
-                                    """
-                                    
-    cur.execute(create_table_querry)
-    conn.commit()
-    
+    try:
+        with database_connection() as conn:
+            with conn.cursor() as cur:
+                querries = [
+                    """CREATE SCHEMA IF NOT EXISTS royal_realtors;""",
+                    """DROP TABLE IF EXISTS royal_realtors.property_listing CASCADE;""",
+                    """DROP TABLE IF EXISTS royal_realtors.property_specification CASCADE;""",
+                    """DROP TABLE IF EXISTS royal_realtors.property_location CASCADE;""",
+                                            
+                    """CREATE TABLE royal_realtors.property_location(
+                        location_id SERIAL PRIMARY KEY,
+                        address VARCHAR (300),
+                        city VARCHAR (150),
+                        county VARCHAR (150),
+                        latitude FLOAT,
+                        longitude FLOAT
+                        );""",
+                    """CREATE TABLE royal_realtors.property_specification(
+                        property_id VARCHAR PRIMARY KEY,
+                        location_id SERIAL,
+                        citnumber_of_rooms INTEGER,
+                        number_of_bathrooms INTEGER,
+                        property_size FLOAT,
+                        year_built DATE,
+                        property_type VARCHAR(100),
+                        lot_size FLOAT,
+                        FOREIGN KEY (location_id) REFERENCES royal_realtors.property_location(location_id)
+                        );""",
+                                            
+                    """CREATE TABLE royal_realtors.property_listing(
+                        listing_id SERIAL PRIMARY KEY,
+                        property_id VARCHAR,
+                        price FLOAT,
+                        listing_type  VARCHAR(150),
+                        listed_date DATE,
+                        status VARCHAR(50),
+                        FOREIGN KEY (property_id) REFERENCES royal_realtors.property_specification(property_id)
+                        );"""
+                        ]
+                for querry in querries:
+                    cur.execute(querry)
+                    logging.info(f"Executed Querry: {querry.split()[:30]}")
+
+    except Exception as e:
+        logging.error(f"connection error:{e}")
+        raise  
